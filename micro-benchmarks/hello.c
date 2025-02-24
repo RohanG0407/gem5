c9551a5fc80ae93c82ac78405ea4f7ca532d7af8
RohanG0407 <gangaraju.rohan@gmail.com>
2025-02-24 15:05:49 -0600

    micro-benchmarks/

--- micro-benchmarks/hello.c
+++ micro-benchmarks/hello.c
@@ -7,33 +7,28 @@ int main(int argc, char *argv[]) {
     long long result = 1;
     long long factor = 2;
 
-    for (int i = 0; i < N * 100; i++) {
-      asm volatile("msr DIT, #1");
-      //asm volatile("ISB");
-      result = 1;
-      for (int j = 0; j < N; j++) {
-          result *= factor;
-      }
-      asm volatile("msr DIT, #0");
-      //asm volatile("ISB");
-    }
-
-    printf("Mult Final result: %lld\n", result);
+    asm volatile("isb");
+    asm volatile("msr DIT, #1");
+    result *= factor;
+    result *= factor;
+    result *= factor;
+    result *= factor;
+    result *= factor;
+    asm volatile("msr DIT, #0");
 
     long long dividend = 1048576;
     long long divisor = 2;
 
-    for (int i = 0; i < N * 100; i++) {
-      asm volatile("msr DIT, #1");
-      //asm volatile("ISB");
-      dividend = 1048576;
-      for (int j = 0; j < N; j++) {
-          dividend /= divisor;
-      }
-      //asm volatile("ISB");
-      asm volatile("msr DIT, #0");
-    }
+    asm volatile("msr DIT, #1");
+    dividend /= divisor;
+    dividend /= divisor;
+    dividend /= divisor;
+    dividend /= divisor;
+    dividend /= divisor;
+    asm volatile("msr DIT, #0");
 
+
+    printf("Mult Final result: %lld\n", result);
     printf("Div Final result: %lld\n", dividend);
 
     return 0;
